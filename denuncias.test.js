b49897eef6e0b0d6672150eaaf5a8d2858e94e1f
DESKTOP-28P4RBF\LENOVO <david_soto123@hotmail.com>
2023-09-30 02:37:34 -0400

    proyecto final de denuncias

--- denuncias.test.js
+++ denuncias.test.js
@@ -1,9 +1,74 @@
-const assert = require('assert');
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from './server';
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+chai.use(chaiHttp);
+
+describe('Denuncias Municipales', () => {
+    it('Devuelve una lista de denuncias', (done) => {
+        chai.request(server)
+            .get('/denuncias')
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200);
+                chai.expect(res.body).to.be.an('array');
+                done();
+            });
     });
-  });
+
+    it('Devuelve una denuncia específica', (done) => {
+        chai.request(server)
+            .get('/denuncias/1')
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200);
+                chai.expect(res.body).to.have.property('id');
+                chai.expect(res.body).to.have.property('titulo');
+                chai.expect(res.body).to.have.property('descripcion');
+                done();
+            });
+    });
+
+    it('Crea una nueva denuncia', (done) => {
+        const body = {
+            titulo: 'Denuncia de prueba',
+            descripcion: 'Esta es una denuncia de prueba.',
+        };
+
+        chai.request(server)
+            .post('/denuncias')
+            .send(body)
+            .end((err, res) => {
+                chai.expect(res).to.have.status(201);
+                chai.expect(res.body).to.have.property('id');
+                chai.expect(res.body).to.have.property('titulo');
+                chai.expect(res.body).to.have.property('descripcion');
+                done();
+            });
+    });
+
+    it('Actualiza una denuncia existente', (done) => {
+        const body = {
+            titulo: 'Denuncia actualizada',
+            descripcion: 'Esta es una denuncia actualizada.',
+        };
+
+        chai.request(server)
+            .patch('/denuncias/1')
+            .send(body)
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200);
+                chai.expect(res.body).to.have.property('id');
+                chai.expect(res.body).to.have.property('titulo');
+                chai.expect(res.body).to.have.property('descripcion');
+                done();
+            });
+    });
+
+    it('Elimina una denuncia existente', (done) => {
+        chai.request(server)
+            .delete('/denuncias/1')
+            .end((err, res) => {
+                chai.expect(res).to.have.status(204);
+                done();
+            });
+    });
 });
